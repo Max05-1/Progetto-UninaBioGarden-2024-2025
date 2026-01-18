0ef3f8110c8ed51d52034d60538660e52c01e2ff
Max05-1 <massimilianocascone125@gmail.com>
2026-01-18 12:27:39 +0100

    Aggiunti metodi alla classe DashboardController

--- UninaBioGarden/src/main/java/controller/DashboardController.java
+++ UninaBioGarden/src/main/java/controller/DashboardController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import dao.ReportDAO;
+import dao.LottoDAO;
+import model.Lotto;
+import model.ReportRaccolta;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class DashboardController {
+
+    private int idProprietario;
+
+    public DashboardController(int idProprietario) {
+        this.idProprietario = idProprietario;
+    }
+
+    //Carica i lotti appartenenti al proprietario:
+    public List<Lotto> getLottiProprietario() throws SQLException {
+        return LottoDAO.getLottiByProprietario(idProprietario);
+    }
+
+    //Carica i dati del report per un lotto specifico:
+    public List<ReportRaccolta> getReportPerLotto(int idLotto) throws SQLException {
+        return ReportDAO.getReportPerLotto(idLotto);
+    }
 }
