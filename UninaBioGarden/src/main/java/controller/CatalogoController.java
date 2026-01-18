c300957c4519aa55c2c24da6bad5de89bca4df97
Max05-1 <massimilianocascone125@gmail.com>
2026-01-18 09:57:48 +0100

    Aggiunti Metodi alla classe CatalogoController

--- UninaBioGarden/src/main/java/controller/CatalogoController.java
+++ UninaBioGarden/src/main/java/controller/CatalogoController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import dao.ColturaDAO;
+import model.ColturaCatalogo;
+import java.sql.SQLException;
+import java.util.List;
+
 public class CatalogoController {
+
+    public CatalogoController() { }
+
+    //Carica l'elenco completo delle colture per il catalogo:
+    public List<ColturaCatalogo> caricaCatalogo() throws SQLException {
+        return ColturaDAO.getCatalogoCompleto();
+    }
 }
