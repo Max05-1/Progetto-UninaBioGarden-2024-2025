7d556b2538db7faab25bbf74849d2f55fce85a69
Max05-1 <massimilianocascone125@gmail.com>
2026-01-18 16:04:39 +0100

    Aggiunti attributi alle classi CatalogoPanel e DashboardPanel

--- UninaBioGarden/src/main/java/view/DashboardPanel.java
+++ UninaBioGarden/src/main/java/view/DashboardPanel.java
@@ -1,4 +1,28 @@
 package view;
 
-public class DashboardPanel {
+import controller.DashboardController;
+import model.Lotto;
+import model.Proprietario;
+import model.ReportRaccolta;
+import util.Theme;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+public class DashboardPanel extends JPanel {
+    //Attributi:
+    private Proprietario proprietario;
+    private JComboBox<Lotto> lottoCombo;
+    private JTable table;
+    private JLabel lblQuantitaMedia;
+    private JLabel lblQuantitaMinima;
+    private JLabel lblQuantitaMassima;
+    private DashboardController controller;
+
+
 }
