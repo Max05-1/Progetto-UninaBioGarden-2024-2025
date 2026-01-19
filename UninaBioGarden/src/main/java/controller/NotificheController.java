fe405fcf77f8deba3ffdd7315b8560f51b903130
vincenzo <candelavincenzo10@gmail.com>
2026-01-19 21:24:21 +0100

    Sistemato problema in NotificheController

--- UninaBioGarden/src/main/java/controller/NotificheController.java
+++ UninaBioGarden/src/main/java/controller/NotificheController.java
@@ -32,6 +32,19 @@ public class NotificheController {
         n.setTipoNotifica("attivita_imminente");
         NotificaDAO.creaNotifica(n);
     }
+
+    public void creaNotificaAnomalia(int idProprietario, int idProgetto, Integer idColtivatore,
+                                     String messaggio, Date dataInvio) throws SQLException {
+        Notifica n = new Notifica();
+        n.setIdProprietario(idProprietario);
+        n.setIdProgetto(idProgetto);
+        n.setIdColtivatore(idColtivatore); // se è null allora è rivolta a tutti
+        n.setMessaggio(messaggio);
+        n.setDataInvio(dataInvio);
+        n.setTipoNotifica("anomalia");
+        NotificaDAO.creaNotifica(n);
+    }
+
     public List<Notifica> caricaNotifichePerProgetto(int idProgetto) throws SQLException {
         return NotificaDAO.getNotificheByProgetto(idProgetto);
     }
